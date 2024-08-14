e88033e7c23805834873b9e0aa892a02c3577838
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2024-08-14 22:57:47 +0300

    Customize theme of the app

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
@@ -23,20 +22,19 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
+    ): View {
         val binding = HomeFragmentBinding.inflate(inflater)
 
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         binding.viewmodel = viewModel
 
-        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer { navigate ->
-            if (navigate){
+        viewModel.navigateToSearch.observe(viewLifecycleOwner) { navigate ->
+            if (navigate) {
                 val navController = binding.root.findNavController()
                 navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
                 viewModel.onNavigatedToSearch()
             }
-        })
+        }
         return binding.root
     }
 }
